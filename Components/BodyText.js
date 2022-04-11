a9cf1c25fd61317988a220df20e937b43f926d2a
kamara Abdulrahman <72914742+abdulrahman-kamara@users.noreply.github.com>
2022-04-11 12:33:30 +0200

    Update BodyText.js

--- Components/BodyText.js
+++ Components/BodyText.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Text, StyleSheet } from "react-native";
 
+// this a text we can use at ant part ofthe body of our appplicatioin
+
 const BodyText = (props) => <Text style={styles.body}>{props.children}</Text>;
 
 const styles = StyleSheet.create({
